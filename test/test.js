867801f605353cebe22625dc77b1d3425d3cdf18
遠藤慎 <end3mame@gmail.com>
2025-05-04 14:11:38 +0900

    feat: beforeEachブロックによる処理の共通化

--- test/test.js
+++ test/test.js
@@ -1,14 +1,18 @@
 const chai = require("chai");
+const { beforeEach } = require("mocha");
 const { sampleProvinceData, Province } = require("./../ch04/sample");
 
 const expect = chai.expect;
 describe("province", function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
+
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
