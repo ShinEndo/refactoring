43135129865ddd47a58bbefcf85fb9ee18adbbf3
遠藤慎 <end3mame@gmail.com>
2025-05-05 12:20:59 +0900

    feat: テストの追加

--- test/ch06/sample04.test.js
+++ test/ch06/sample04.test.js
@@ -1,12 +1,19 @@
 const chai = require("chai");
-const { getDefaultOwner } = require("../../ch06/sample04");
+const { getDefaultOwner, setDefaultOwner } = require("../../ch06/sample04");
 
 const expect = chai.expect;
 describe("ch06 変数のカプセル化", function () {
-  it("defaultOwner", function () {
+  it("defaultOwner - get", function () {
     const spaceship = {};
     spaceship.owner = getDefaultOwner();
     expect(spaceship.owner.firstName).equal("Martin");
     expect(spaceship.owner.lastName).equal("Fowler");
+  });
+  it("defaultOwner - set", function () {
+    setDefaultOwner({ firstName: "Rebecca", lastName: "Persons" });
+    const spaceship = {};
+    spaceship.owner = getDefaultOwner();
+    expect(spaceship.owner.firstName).equal("Rebecca");
+    expect(spaceship.owner.lastName).equal("Persons");
   });
 });
