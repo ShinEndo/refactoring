a1728590b6296149ec5e2b4825930471d07efd04
遠藤慎 <end3mame@gmail.com>
2025-05-06 08:50:16 +0900

    refactor: applyShipping関数の抽出

--- ch06/sample05.js
+++ ch06/sample05.js
@@ -1,13 +1,18 @@
-export default function priceOrder(product, quantity, shippingMehod) {
+export default function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+
+  return price;
+}
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase =
-    basePrice > shippingMehod.discountThreshold
-      ? shippingMehod.discountedFee
-      : shippingMehod.freePerCase;
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.freePerCase;
   const shippingCost = quantity * shippingPerCase;
   const price = basePrice - discount + shippingCost;
   return price;
