526704bf4452cffdb568b9d73a2237fcc283954e
遠藤慎 <end3mame@gmail.com>
2025-05-05 12:21:37 +0900

    refactor: 変数・関数名の変更

--- ch06/sample04.js
+++ ch06/sample04.js
@@ -1,7 +1,7 @@
-let defaultOwner = { firstName: "Martin", lastName: "Fowler" };
-export function getDefaultOwner() {
-  return defaultOwner;
+let defaultOwnerData = { firstName: "Martin", lastName: "Fowler" };
+export function defaultOwner() {
+  return defaultOwnerData;
 }
 export function setDefaultOwner(arg) {
-  defaultOwner = arg;
+  defaultOwnerData = arg;
 }
--- test/ch06/sample04.test.js
+++ test/ch06/sample04.test.js
@@ -1,18 +1,18 @@
 const chai = require("chai");
-const { getDefaultOwner, setDefaultOwner } = require("../../ch06/sample04");
+const { defaultOwner, setDefaultOwner } = require("../../ch06/sample04");
 
 const expect = chai.expect;
 describe("ch06 変数のカプセル化", function () {
   it("defaultOwner - get", function () {
     const spaceship = {};
-    spaceship.owner = getDefaultOwner();
+    spaceship.owner = defaultOwner();
     expect(spaceship.owner.firstName).equal("Martin");
     expect(spaceship.owner.lastName).equal("Fowler");
   });
   it("defaultOwner - set", function () {
     setDefaultOwner({ firstName: "Rebecca", lastName: "Persons" });
     const spaceship = {};
-    spaceship.owner = getDefaultOwner();
+    spaceship.owner = defaultOwner();
     expect(spaceship.owner.firstName).equal("Rebecca");
     expect(spaceship.owner.lastName).equal("Persons");
   });
