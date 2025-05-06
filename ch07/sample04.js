a60b4181d532cc36939de5b9bbdce5593279b18e
遠藤慎 <end3mame@gmail.com>
2025-05-06 11:06:51 +0900

    feat: クラスのインライン化
    リファクタリング前のサンプルコード、テストコードの作成

---
+++ ch07/sample04.js
@@ -0,0 +1,27 @@
+export class TrackingInformation {
+	get shippingCompany() {
+		return this._shippingCompany
+	}
+	set shippingCompany(arg) {
+		this._shippingCompany = arg;
+	}
+	get trackingNumber() {return this._trackingNumber;}
+	set trackingNumber(arg) {this._trackingNumber = arg;}
+	get display() {
+		return `${this._shippingCompany}: ${this._trackingNumber}`;
+	}
+}
+
+export class Shipment {
+
+	constructor() {
+		this._trackingInformation = new TrackingInformation();
+	}
+	get trackingInfo() {
+		return this._trackingInformation.display;
+	}
+	get trackingInformation() {return this._trackingInformation;}
+	set trackingInformation(aTrackingInformation) {
+		this._trackingInformation = aTrackingInformation;
+	}
+}
---
+++ test/ch07/sample04.test.js
@@ -0,0 +1,14 @@
+const chai = require("chai");
+const { TrackingInformation, Shipment } = require("../../ch07/sample04");
+
+const expect = chai.expect;
+describe("ch07 クラスのインライン化", function () {
+  it("TrackingInformation", function () {
+    const shipment = new Shipment();
+    shipment.trackingInformation.shippingCompany = 'Amazon';
+    shipment.trackingInformation.trackingNumber = '00-0000-0000';
+    expect(shipment.trackingInformation.shippingCompany).equal('Amazon');
+    expect(shipment.trackingInformation.trackingNumber).equal('00-0000-0000');
+    expect(shipment.trackingInformation.display).equal('Amazon: 00-0000-0000');
+  });
+});
