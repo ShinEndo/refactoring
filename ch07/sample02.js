909bba114ce73a87ebd31f11e061a918d032f633
遠藤慎 <end3mame@gmail.com>
2025-05-06 09:58:26 +0900

    feat: オブジェクトによるプリミティブの置き換え
    リファクタリング前のサンプルコード、テストコードの作成

---
+++ ch07/sample02.js
@@ -0,0 +1,5 @@
+export default class Order {
+  constructor(data) {
+    this.priority = data.priority;
+  }
+}
---
+++ test/ch07/sample02.test.js
@@ -0,0 +1,33 @@
+const chai = require("chai");
+const { default: Order } = require("../../ch07/sample02");
+
+const expect = chai.expect;
+describe("ch07 オブジェクトによるプリミティブの置き換え", function () {
+  it("Order", function () {
+    const datas = [
+      {
+        priority: "high",
+      },
+      {
+        priority: "rush",
+      },
+      {
+        priority: "low",
+      },
+      {
+        priority: "high",
+      },
+      {
+        priority: "rush",
+      },
+      {
+        priority: "low",
+      },
+    ];
+    const orders = datas.map((data) => new Order(data));
+    const highPriorityCount = orders.filter(
+      (o) => "high" === o.priority || "rush" === o.priority
+    ).length;
+    expect(highPriorityCount).equal(4);
+  });
+});
