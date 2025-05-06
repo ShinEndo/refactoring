a404bf952cdea822c5ef7fe5fa64d297353765cc
遠藤慎 <end3mame@gmail.com>
2025-05-06 10:00:29 +0900

    feat: 変数のカプセル化用関数の追加

--- ch07/sample02.js
+++ ch07/sample02.js
@@ -2,4 +2,10 @@ export default class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+  get priority() {
+    return this.priority;
+  }
+  set priority(aString) {
+    this.priority = aString;
+  }
 }
