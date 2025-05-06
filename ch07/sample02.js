dea8c5c23b7d0400a79370f37eaf2ad5af283030
遠藤慎 <end3mame@gmail.com>
2025-05-06 10:25:02 +0900

    feat: 優先度の値をバリデーションするロジックと、比較するロジックの追加

--- ch07/sample02.js
+++ ch07/sample02.js
@@ -1,4 +1,4 @@
-export default class Order {
+export class Order {
   constructor(data) {
     this.priority = data.priority;
   }
@@ -13,12 +13,29 @@ export default class Order {
   }
 }
 
-class Priority {
+export class Priority {
   constructor(value) {
     if (value instanceof Priority) return value;
-    this._value = value;
+    if (Priority.legalValues().includes(value)) this._value = value;
+    else throw new Error(`<${value}> is invalid for Priprity`);
   }
   toString() {
     return this._value;
+  }
+  get _index() {
+    return Priority.legalValues().findIndex((s) => s === this._value);
+  }
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
+  }
+
+  equals(other) {
+    return this._index === other._index;
+  }
+  higherThan(other) {
+    return this._index > other._index;
+  }
+  lowerThan(other) {
+    return this._index < other._index;
   }
 }
--- test/ch07/sample02.test.js
+++ test/ch07/sample02.test.js
@@ -1,5 +1,5 @@
 const chai = require("chai");
-const { default: Order } = require("../../ch07/sample02");
+const { Order, Priority } = require("../../ch07/sample02");
 
 const expect = chai.expect;
 describe("ch07 オブジェクトによるプリミティブの置き換え", function () {
@@ -25,10 +25,36 @@ describe("ch07 オブジェクトによるプリミティブの置き換え", fu
       },
     ];
     const orders = datas.map((data) => new Order(data));
-    console.log(orders);
     const highPriorityCount = orders.filter(
       (o) =>
         "high" === o.priority.toString() || "rush" === o.priority.toString()
+    ).length;
+    expect(highPriorityCount).equal(4);
+  });
+  it("Order - higherThan ", function () {
+    const datas = [
+      {
+        priority: "high",
+      },
+      {
+        priority: "rush",
+      },
+      {
+        priority: "low",
+      },
+      {
+        priority: "high",
+      },
+      {
+        priority: "rush",
+      },
+      {
+        priority: "low",
+      },
+    ];
+    const orders = datas.map((data) => new Order(data));
+    const highPriorityCount = orders.filter((o) =>
+      o.priority.higherThan(new Priority("normal"))
     ).length;
     expect(highPriorityCount).equal(4);
   });
