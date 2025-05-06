8622200ce75447897a20bf01ea7ed8ef065ab506
遠藤慎 <end3mame@gmail.com>
2025-05-06 10:44:15 +0900

    refactor: basePrice関数の抽出、変数のインライン化

--- ch07/sample03.js
+++ ch07/sample03.js
@@ -4,9 +4,11 @@ export default class Order {
     this._item = item;
   }
   get price() {
-    const basePrice = this._quantity * this._item.price;
     var discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return this.basePrice * discountFactor;
+  }
+  get basePrice() {
+    return this._quantity * this._item.price;
   }
 }
