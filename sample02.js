624f7d5f91fc38837fe140856be907e3bb09a8f9
endo <endo@elplanning.co.jp>
2025-05-09 12:49:47 +0900

    refactor: 委譲呼び出しに置き換え

--- sample02.js
+++ sample02.js
@@ -2,17 +2,11 @@ class Account {
   constructor() {}
   get bankCharge() {
     let result = 4.5;
-    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+    if (this._daysOverdrawn > 0) result += this.type.overdraftCharge(this.daysOverdrawn);
     return result;
   }
   get overdraftCharge() {
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this.daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + (this.daysOverdrawn - 7) * 0.85;
-    } else {
-      return this.daysOverdrawn * 1.75;
-    }
+    return this.type.overdraftCharge(this.daysOverdrawn);
   }
 }
 
