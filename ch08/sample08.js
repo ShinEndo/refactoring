421653636fc94c7cb282de33f6d1bda01d5a823b
遠藤慎 <end3mame@gmail.com>
2025-05-10 11:01:31 +0900

    refactor: パイプライン化によるsplit関数の削除

--- ch08/sample08.js
+++ ch08/sample08.js
@@ -3,9 +3,10 @@ export function acquireData(input) {
 	const result = [];
 	const loopItem = lines
 		.slice(1)
-		.filter(line => line.trim() !== '');
+		.filter(line => line.trim() !== '')
+		.map(line => line.split(","));
 	for(const line of loopItem) {
-		const record = line.split(",");
+		const record = line;
 		if(record[1].trim() === "India") {
 			result.push({city: record[0].trim(), phone: record[2].trim()});
 		}
