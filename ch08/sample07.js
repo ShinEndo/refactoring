55be57bf57aca8128bfbee0c7b054abee9514d83
遠藤慎 <end3mame@gmail.com>
2025-05-10 10:29:40 +0900

    refactor: youngestAge関数の抽出

--- ch08/sample07.js
+++ ch08/sample07.js
@@ -1,15 +1,18 @@
 function sampleFunction() {
-
-	let youngest = people[0] ? people[0].age : Infinity;
-	for(const p of people) {
-		if(p.age < youngest) youngest = p.age;
-	}
-	return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+	return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
 
 	function totalSalary() {
 		let totalSalary = 0;
 		for(const p of people) {
 			totalSalary += p.salary;
 		}
+		return totalSalary;
+	}
+	function youngestAge() {
+		let youngest = people[0] ? people[0].age : Infinity;
+		for(const p of people) {
+			if(p.age < youngest) youngest = p.age;
+		}
+		return youngest;
 	}
 }
