bedad147214a1404af880ed715a1c519d1c8cdaf
遠藤慎 <end3mame@gmail.com>
2025-05-10 09:53:14 +0900

    feat: ステートメントの呼び出し側への移動
    リファクタリング前のサンプルコードの作成

---
+++ ch08/sample06.js
@@ -0,0 +1,22 @@
+function renderPerson(outStream,person) {
+	outStream.write(`<p>${person.name}</p>\n`);
+	renderPhoto(outStream,person.photo);
+	emitPhotoData(outStream,person.photo);
+}
+
+function listRecentPhotos(outStream,photos) {
+	photos
+		.filter(p => p.date > recentDateCutoff())
+		.forEach(p => {
+			outStream.wite("<div>\n");
+			emitPhotoData(outStream,p);
+			outStream.wite("</div>\n");
+		});
+}
+
+function emitPhotoData(outStream,photo) {
+	outStream.write(`<p>title: ${photo.title}</p>\n`);
+	outStream.write(`<p>location: ${photo.location}</p>\n`);
+	outStream.write(`<p>date: ${photo.toDateString()}</p>\n`);
+
+}
