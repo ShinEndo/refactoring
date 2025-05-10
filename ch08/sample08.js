b014bad0bf1f1c2c52029eeb53a54d8bd8c6dc3e
遠藤慎 <end3mame@gmail.com>
2025-05-10 10:58:39 +0900

    refactor: パイプライン化による制御用変数の削除

--- ch08/sample08.js
+++ ch08/sample08.js
@@ -1,14 +1,8 @@
 export function acquireData(input) {
 	const lines = input.split("\n");
-	let firstLine = true;
 	const result = [];
-	const loopItem = lines;
+	const loopItem = lines.slice(1).filter(line => line.trim() !== '');
 	for(const line of loopItem) {
-		if(firstLine) {
-			firstLine = false;
-			continue;
-		}
-		if(line.trim() === "") continue;
 		const record = line.split(",");
 		if(record[1].trim() === "India") {
 			result.push({city: record[0].trim(), phone: record[2].trim()});
