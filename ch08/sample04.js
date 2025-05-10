d95619fabf672e38eea22baba4d12ba281c7e068
遠藤慎 <end3mame@gmail.com>
2025-05-10 09:09:48 +0900

    refactor: アサーションの導入

--- ch08/sample04.js
+++ ch08/sample04.js
@@ -2,13 +2,16 @@ class Account {
 	constructor(number, type, interestRate) {
 		this._number = number;
 		this._type = type;
+		assert(interestRate === this._type.interestRate);
 		this._interesetRate = interestRate;
 	}
-	get interestRate() { return this.interestRate; }
+	get interestRate() { return this._type.interestRate; }
 }
 
 class AccountType {
-	constructor(nameString) {
+	constructor(nameString, interestRate) {
 		this._name = nameString;
+		this._interesetRate = interestRate;
 	}
+	get interestRate() { return this._interesetRate; }
 }
