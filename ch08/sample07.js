c54694915f1b6b6a5e7646fa1ef66f61a8ada6d8
遠藤慎 <end3mame@gmail.com>
2025-05-10 10:32:54 +0900

    refactor: アルゴリズムの置き換え

--- ch08/sample07.js
+++ ch08/sample07.js
@@ -5,10 +5,6 @@ function sampleFunction() {
 		return people.reduce((total,p) => total + p.salary, 0);
 	}
 	function youngestAge() {
-		let youngest = people[0] ? people[0].age : Infinity;
-		for(const p of people) {
-			if(p.age < youngest) youngest = p.age;
-		}
-		return youngest;
+		return Math.min(...people.map(p => p.age));
 	}
 }
