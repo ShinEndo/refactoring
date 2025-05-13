bbf7efeb88504b2469ade862ff9ba976b98caefb
遠藤慎 <end3mame@gmail.com>
2025-05-13 22:07:03 +0900

    refactor: ガード節入れる条件判定の逆転

--- ch10/sample02.js
+++ ch10/sample02.js
@@ -1,9 +1,8 @@
 function adjustedCapital(anInrtrument) {
 	let result = 0;
-	if(anInrtrument.capital > 0) {
-		if(anInrtrument.interestRate > 0 && anInrtrument.duration > 0) {
-			result = (anInrtrument.income / anInrtrument.duration) * anInrtrument.adjustmentFactor;
-		}
-		return result;
+	if(anInrtrument <= 0) return result;
+	if(anInrtrument.interestRate > 0 && anInrtrument.duration > 0) {
+		result = (anInrtrument.income / anInrtrument.duration) * anInrtrument.adjustmentFactor;
 	}
+	return result;
 }
