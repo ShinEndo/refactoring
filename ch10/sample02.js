24d618dd5e47d58c7146add4afc9827eb945d980
遠藤慎 <end3mame@gmail.com>
2025-05-13 22:11:11 +0900

    refactor: result変数の削除

--- ch10/sample02.js
+++ ch10/sample02.js
@@ -1,8 +1,6 @@
 function adjustedCapital(anInrtrument) {
-	let result = 0;
 	if(		anInrtrument 								<= 0
 	 		|| anInrtrument.interestRate	<= 0
 			|| anInrtrument.duration			<= 0 ) return result;
-	result = (anInrtrument.income / anInrtrument.duration) * anInrtrument.adjustmentFactor;
-	return result;
+	return (anInrtrument.income / anInrtrument.duration) * anInrtrument.adjustmentFactor;
 }
