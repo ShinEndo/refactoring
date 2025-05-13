35a8b85bf9da960c055f426f1cbb6f9083a59ae0
遠藤慎 <end3mame@gmail.com>
2025-05-13 22:36:56 +0900

    refactor: switch文の抽出

--- ch10/sample03.js
+++ ch10/sample03.js
@@ -38,16 +38,7 @@ class Bird {
   }
 
   get airSpeedVelocity() {
-    switch(this.type) {
-      case 'EuropeanSwallow':
-        return 35;
-      case 'AfricanSwallow':
-        return 40 - 2 * this.numberOfCoconuts;
-      case 'NorwegianBlueParrot':
-        return (this.inNailed) ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+    return null;
   }
 }
 
@@ -55,16 +46,25 @@ class EuropeanSwallow extends Bird {
   get plumage() {
     return 'average';
   }
+  get airSpeedVelocity() {
+    return 35;
+  }
 }
 
 class AfricanSwallow extends Bird{
   get plumage() {
     return (this.numberOfCoconuts > 2) ? "tired" : "average";
   }
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
 }
 
 class NorwegianBlueParrot extends Bird{
   get plumage() {
     return (this.voltage > 100) ? "scorched" : "beautiful";
+  }
+  get airSpeedVelocity() {
+    return (this.inNailed) ? 0 : 10 + this.voltage / 10;
   }
 }
