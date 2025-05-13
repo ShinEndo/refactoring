d8f130719c9650dd5b5eebd782f96a68689672a5
遠藤慎 <end3mame@gmail.com>
2025-05-13 21:58:46 +0900

    refactor: 入れ子を1つ減らす

--- ch10/sample01.js
+++ ch10/sample01.js
@@ -1,18 +1,15 @@
 function payAmount(employee) {
   let result;
-  if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: "SEP" };
+  if (employee.isSeparated) return { amount: 0, reasonCode: "SEP" };
+  if (employee.isRetired) {
+    result = { amount: 0, reasonCode: "RET" };
   } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: "RET" };
-    } else {
-      // 金額を計算するロジック
-      lorem.ipsum(dolor.sitAmet);
-      consectetur(adipiscing).elit();
-      sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(MessageChannel.aliqua);
-      ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-    }
+    // 金額を計算するロジック
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(MessageChannel.aliqua);
+    ut.enim.ad(minim.veniam);
+    result = someFinalComputation();
   }
   return result;
 }
