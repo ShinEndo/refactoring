f1810be766c933e52b2c5d32a3fc3201c4087c81
遠藤慎 <end3mame@gmail.com>
2025-05-13 22:07:59 +0900

    refactor: notによる条件判定の逆転

--- ch10/sample02.js
+++ ch10/sample02.js
@@ -1,8 +1,7 @@
 function adjustedCapital(anInrtrument) {
 	let result = 0;
 	if(anInrtrument <= 0) return result;
-	if(anInrtrument.interestRate > 0 && anInrtrument.duration > 0) {
-		result = (anInrtrument.income / anInrtrument.duration) * anInrtrument.adjustmentFactor;
-	}
+	if(!(anInrtrument.interestRate > 0 && anInrtrument.duration > 0)) return result;
+	result = (anInrtrument.income / anInrtrument.duration) * anInrtrument.adjustmentFactor;
 	return result;
 }
