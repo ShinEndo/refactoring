bfe54ec27ecaed51e639be47af63be27864584e2
遠藤慎 <end3mame@gmail.com>
2025-05-13 22:10:37 +0900

    refactor: 条件記述の結合

--- ch10/sample02.js
+++ ch10/sample02.js
@@ -1,7 +1,8 @@
 function adjustedCapital(anInrtrument) {
 	let result = 0;
-	if(anInrtrument <= 0) return result;
-	if(anInrtrument.interestRate <= 0 || anInrtrument.duration <= 0) return result;
+	if(		anInrtrument 								<= 0
+	 		|| anInrtrument.interestRate	<= 0
+			|| anInrtrument.duration			<= 0 ) return result;
 	result = (anInrtrument.income / anInrtrument.duration) * anInrtrument.adjustmentFactor;
 	return result;
 }
