19e3a4b58eb964137034c4d0d62586d40471504b
endo <endo@elplanning.co.jp>
2025-05-14 12:47:32 +0900

    refactor: 関数の抽出、サブクラスへの移動

--- ch10/sample04.js
+++ ch10/sample04.js
@@ -1,5 +1,5 @@
 export function rating(voyage, history) {
-  return new Rating(voyage, history).value;
+  return createRating(voyage, history).value;
 }
 
 function createRating(voyage, history) {
@@ -33,7 +33,6 @@ class Rating {
     let result = 1;
     if (this.history.length < 5) result += 4;
     result += this.history.filter((v) => v.profit < 0).length;
-    if (this.voyage.zone === "china" && this.hasChinaHistory(this.history)) result -= 2;
     return Math.max(result, 0);
   }
 
@@ -41,15 +40,14 @@ class Rating {
     let result = 2;
     if (this.voyage.zone === "china") result += 1;
     if (this.voyage.zone === "east-indies") result += 1;
-    if (this.voyage.zone === "china" && this.hasChinaHistory(this.history)) {
-      result += 3;
-      if (this.history.length > 10) result += 1;
-      if (this.voyage.length > 12) result += 1;
-      if (this.voyage.length > 18) result -= 1;
-    } else {
-      if (this.history.length > 8) result += 1;
-      if (this.history.length > 14) result -= 1;
-    }
+    result += this.voyageAndHistorylengthFactor;
+    return result;
+  }
+
+  get voyageAndHistorylengthFactor() {
+    let result = 0;
+    if (this.history.length > 8) result += 1;
+    if (this.history.length > 14) result -= 1;
     return result;
   }
 
@@ -58,4 +56,18 @@ class Rating {
   }
 }
 
-class ExperiencedChinaRating extends Rating {}
+class ExperiencedChinaRating extends Rating {
+  get captainHistoryRisk() {
+    const result = super.captainHistoryRisk - 2;
+    return Math.max(result, 0);
+  }
+
+  get voyageAndHistorylengthFactor() {
+    let result = 0;
+    result += 3;
+    if (this.history.length > 10) result += 1;
+    if (this.voyage.length > 12) result += 1;
+    if (this.voyage.length > 18) result -= 1;
+    return result;
+  }
+}
