a06be65a0364b0aaebe38458c76a355764d40c31
遠藤慎 <end3mame@gmail.com>
2025-05-17 10:46:41 +0900

    refactor: デッドコードの削除

--- test/ch11/sample03.test.js
+++ test/ch11/sample03.test.js
@@ -11,15 +11,11 @@ describe("ch11 オブジェクトそのものの受け渡し", function () {
 
   it("withinRange - True", function () {
     const aRoom = {daysTempRange: {low: 22, high: 24}};
-    const low = aRoom.daysTempRange.low;
-    const high = aRoom.daysTempRange.high;
     expect(aPlan.xxNEWwithinRange(aRoom.daysTempRange)).equal(true);
   });
 
   it("withinRange - False", function () {
     const aRoom = {daysTempRange: {low: 18, high: 25}};
-    const low = aRoom.daysTempRange.low;
-    const high = aRoom.daysTempRange.high;
     expect(aPlan.xxNEWwithinRange(aRoom.daysTempRange)).equal(false);
   });
 });
