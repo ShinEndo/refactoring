bd288c6e6e28d73d39d03684de6ab7e45da7d52e
遠藤慎 <end3mame@gmail.com>
2025-05-17 11:03:42 +0900

    refactor: 問い合わせによる一時変数の置き換え

--- ch11/sample04.js
+++ ch11/sample04.js
@@ -1,10 +1,14 @@
 class Order {
 	get finalPrice() {
 		const basePrice = this.quantity * this.itemPrice;
+		return this.discountedPrice(basePrice, this.discountLevel);
+	}
+
+	discountLevel() {
 		let discountLevel;
 		if(this.quantity > 100) discountLevel = 2;
 		else discountLevel = 1;
-		return this.discountedPrice(basePrice, discountLevel);
+		return discountLevel;
 	}
 
 	discountedPrice(basePrice, discountLevel) {
