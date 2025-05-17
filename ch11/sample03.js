0946dfe9ecc19b7f80abc5616ebc506c1887015e
遠藤慎 <end3mame@gmail.com>
2025-05-17 10:48:43 +0900

    refactor: 不要関数の削除

--- ch11/sample03.js
+++ ch11/sample03.js
@@ -5,9 +5,6 @@ export class HeatingPlan {
 			high: 25,
 		}
 	}
-	withinRange(bottom,top) {
-		return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
-	}
 
 	xxNEWwithinRange(aNumberRange) {
 		return (aNumberRange.low >= this._temperatureRange.low) && (aNumberRange.high <= this._temperatureRange.high);
