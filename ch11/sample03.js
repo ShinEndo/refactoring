680cc665c30447b0acc7e21eb0ef6527be63894b
遠藤慎 <end3mame@gmail.com>
2025-05-17 10:49:19 +0900

    refactor: 関数名の一括置換

--- ch11/sample03.js
+++ ch11/sample03.js
@@ -1,12 +1,15 @@
 export class HeatingPlan {
-	constructor() {
-		this._temperatureRange = {
-			low: 20,
-			high: 25,
-		}
-	}
+  constructor() {
+    this._temperatureRange = {
+      low: 20,
+      high: 25,
+    };
+  }
 
-	xxNEWwithinRange(aNumberRange) {
-		return (aNumberRange.low >= this._temperatureRange.low) && (aNumberRange.high <= this._temperatureRange.high);
-	}
+  withinRange(aNumberRange) {
+    return (
+      aNumberRange.low >= this._temperatureRange.low &&
+      aNumberRange.high <= this._temperatureRange.high
+    );
+  }
 }
--- test/ch11/sample03.test.js
+++ test/ch11/sample03.test.js
@@ -3,19 +3,18 @@ const { HeatingPlan } = require("../../ch11/sample03");
 
 const expect = chai.expect;
 describe("ch11 オブジェクトそのものの受け渡し", function () {
-
   let aPlan;
   beforeEach(function () {
     aPlan = new HeatingPlan();
   });
 
   it("withinRange - True", function () {
-    const aRoom = {daysTempRange: {low: 22, high: 24}};
-    expect(aPlan.xxNEWwithinRange(aRoom.daysTempRange)).equal(true);
+    const aRoom = { daysTempRange: { low: 22, high: 24 } };
+    expect(aPlan.withinRange(aRoom.daysTempRange)).equal(true);
   });
 
   it("withinRange - False", function () {
-    const aRoom = {daysTempRange: {low: 18, high: 25}};
-    expect(aPlan.xxNEWwithinRange(aRoom.daysTempRange)).equal(false);
+    const aRoom = { daysTempRange: { low: 18, high: 25 } };
+    expect(aPlan.withinRange(aRoom.daysTempRange)).equal(false);
   });
 });
