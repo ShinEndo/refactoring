34b8f7e43d6fa49e39beb04a3ca93b2552ed57f3
遠藤慎 <end3mame@gmail.com>
2025-05-18 13:45:20 +0900

    refactor: ファクトリメソッドでスーパークラスを返すように変更

--- ch12/sample04.js
+++ ch12/sample04.js
@@ -47,7 +47,7 @@ export function createBooking(show,date) {
 }
 
 export function createPremiumBooking(show,date,extras) {
-	const result = new PremiumBooking(show,date,extras);
+	const result = new Booking(show,date,extras);
 	result._bePremium(extras);
 	return result;
 }
