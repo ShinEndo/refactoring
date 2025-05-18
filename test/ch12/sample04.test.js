02360813c6067d87478e1b3ba6680b9395dd7214
遠藤慎 <end3mame@gmail.com>
2025-05-18 13:25:04 +0900

    refactor: デッドコードの削除

--- test/ch12/sample04.test.js
+++ test/ch12/sample04.test.js
@@ -1,12 +1,11 @@
 const chai = require("chai");
-const { Booking, PremiumBooking, createBooking, createPremiumBooking } = require("../../ch12/sample04");
+const { createBooking, createPremiumBooking } = require("../../ch12/sample04");
 
 const expect = chai.expect;
 describe("ch12 委譲によるサブクラスの置き換え", function () {
 	it("Booking", function () {
 		const show = { price: 100, talkback: true };
 		const date = '2025-05-18';
-		// const booking = new Booking(show,date);
 		const booking = createBooking(show,date);
 		booking.isPeakDay = true;
 		expect(booking.basePrice).equal(115);
@@ -15,7 +14,6 @@ describe("ch12 委譲によるサブクラスの置き換え", function () {
 	it("Booking - Not PeakDay", function () {
 		const show = { price: 100, talkback: true };
 		const date = '2025-05-18';
-		// const booking = new Booking(show,date);
 		const booking = createBooking(show,date);
 		expect(booking.basePrice).equal(100);
 		expect(booking.hasTalkBack).equal(true);
@@ -24,7 +22,6 @@ describe("ch12 委譲によるサブクラスの置き換え", function () {
 		const show = { price: 100, talkback: true };
 		const date = '2025-05-18';
 		const extras = { premiumFee: 50,}
-		// const booking = new PremiumBooking(show,date,extras);
 		const booking = createPremiumBooking(show,date,extras);
 		booking.isPeakDay = true;
 		expect(booking.basePrice).equal(165);
@@ -35,7 +32,6 @@ describe("ch12 委譲によるサブクラスの置き換え", function () {
 		const show = { price: 100, talkback: true };
 		const date = '2025-05-18';
 		const extras = { premiumFee: 50, dinner: true}
-		// const booking = new PremiumBooking(show,date,extras);
 		const booking = createPremiumBooking(show,date,extras);
 		booking.isPeakDay = false;
 		expect(booking.basePrice).equal(150);
